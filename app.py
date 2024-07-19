ee973efb92b206df1cf5c882e15a6f3afaf3907f
Joe Haddad <joe@sefactory.io>
2024-07-19 22:51:40 +0300

    Adjusted pylint errors

--- app.py
+++ app.py
@@ -1,31 +1,47 @@
+"""
+A Streamlit app to find recipes based on ingredients.
+"""
+
 import streamlit as st
 import requests
 
 
 st.title("Recipe Finder")
 
-
-ingredients = st.text_input("Enter ingredients (comma separated)")
-
+# Input field for ingredients
+ingredients_input = st.text_input("Enter ingredients (comma separated)")
 
 def get_recipes(ingredients):
-    url = f"https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients}&number=5&apiKey=3a0aaf1dccae480c9da6d06e1ea617d4"
-    response = requests.get(url)
+    """
+    Fetch recipes based on ingredients from the Spoonacular API.
+    
+    Args:
+    ingredients (str): Comma-separated list of ingredients.
+    
+    Returns:
+    list: A list of recipes.
+    """
+    api_key = "3a0aaf1dccae480c9da6d06e1ea617d4"
+    url = (
+        f"https://api.spoonacular.com/recipes/findByIngredients?"
+        f"ingredients={ingredients}&number=5&apiKey={api_key}"
+    )
+    response = requests.get(url, timeout=10)  # Added timeout for the request
     return response.json()
 
-
 if st.button("Search"):
-    if ingredients:
-        recipes = get_recipes(ingredients)
-        
+    if ingredients_input:
+        recipes = get_recipes(ingredients_input)
+
         if recipes:
             for recipe in recipes:
                 st.subheader(recipe['title'])
                 st.image(recipe['image'])
-                
+
                 st.write("**Missing Ingredients:**")
                 for ingredient in recipe['missedIngredients']:
-                    st.write(f"{ingredient['name'].capitalize()} - {ingredient['amount']} {ingredient['unit']}")
+                    st.write(f"{ingredient['name'].capitalize()} - "
+                             f"{ingredient['amount']} {ingredient['unit']}")
                     st.image(ingredient['image'])
         else:
             st.write("No recipes found.")
